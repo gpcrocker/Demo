ec1de8b555b0bfadcf0ef00a5edd68740944a1f0
gpcrocker88 <gpcrocker@gmail.com>
2016-01-03 15:17:55 +0000

    Added Configuration

---
+++ config/ConfLoader.php
@@ -0,0 +1,29 @@
+<?php
+namespace Gpcrocker88\Config;
+
+
+class ConfLoader
+{
+    const CONF_FNAME  = "conf.ini";
+    private $conf;
+
+    /**
+ *      * ConfLoader constructor.
+ *           */
+    public function __construct($conf_file=null)
+    {
+        if(empty($conf_file)) {
+            $this->conf = parse_ini_file(self::CONF_FNAME, true);
+        }else{
+            $this->conf = parse_ini_file($conf_file, true);
+        }
+    }
+
+
+    public function getDBInfo()
+    {
+        return $this->conf["db"];
+    }
+
+}
+
